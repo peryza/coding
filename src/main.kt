5e9816a471d5b812f495c2d23c5315d2f64b362b
peryza <51073553+peryza@users.noreply.github.com>
2020-10-15 21:35:35 +0700

    #2 Create .gitignore

--- src/main.kt
+++ src/main.kt
@@ -1,61 +1,61 @@
-
-fun sorted(list: List<String>): List<String>{
-    var ListSorted = list.sorted()
+fun sorted(list: List<String>): List<String> {
+    val ListSorted = list.sorted()
     return ListSorted
 }
 
 
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
 
-    var words = readLine().toString()
-
+    val words = readLine().toString()
+//   Разделение строк
     println("-----------Первый уровень-------------------")
     var yes = words.split(" ")
-    yes.forEach { el -> println(el) } //вывод списка по одному слову
-//    Сортировка списка
+    yes.forEach { el -> println(el) }
+
+
+//    Сортировка списка слов
     println("-----------Второй уровень---------------------")
     yes = sorted(yes)
-    yes.forEach { el -> println(el) } // вывод списка
+    yes.forEach { el -> println(el) }
 
+// вывод только уникальных слов
     println("-----------Третий уровень---------------------")
 
     var i = 0
-    var count = yes.size-1
-    while ( i < count ){
-        if (yes[i] != yes[i+1]){
+    var count = yes.size - 1
+    while (i < count) {
+        if (yes[i] != yes[i + 1]) {
             println(yes[i])
 
         }
-        i+= 1
+        i += 1
     }
     println(yes.last())
 
+//Подсчет количества повторений слов
     println("----------Четвертый уровень-----------------")
+
     i = 0
     var k = 0
-    var yes2 = mutableMapOf<String, Int>() // создание ассоциативного массива
+    val yes2 = mutableMapOf<String, Int>() // создание ассоциативного массива
 
-    count = yes.size-1
-    while ( i < count ){
-        if (yes[i] != yes[i+1]) {
-            yes2.put(yes[i], k+1)
+    count = yes.size - 1
+    while (i < count) {
+        if (yes[i] != yes[i + 1]) {
+            yes2.put(yes[i], k + 1)
 
-            k=0
-        }else k+= 1
+            k = 0
+        } else k += 1
 
-        i+= 1}
-    yes2.put(yes.last(), k+1)
-    yes2.forEach{key, value -> println("$key $value")}
+        i += 1
+    }
+    yes2.put(yes.last(), k + 1)
+    yes2.forEach { (key, value) -> println("$key $value") }
 
-
+//    сортировка слов по убыванию
     println("----------Пятый уровень----------------")
-
-//    сортировка по убыванию чисел
-    val result = yes2.toList().sortedByDescending { (_,value)->value }.toMap()
-    result.forEach{key, value -> println("$key $value")}
-
-
-
+    val result = yes2.toList().sortedByDescending { (_, value) -> value }.toMap()
+    result.forEach { key, value -> println("$key $value") }
 
 
 }
