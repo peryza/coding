ef270d136b63918d568341f4b6091e732975dfe8
peryza <51073553+peryza@users.noreply.github.com>
2020-10-16 23:17:48 +0700

    #5 code formatting

--- TEST.bat
+++ TEST.bat
@@ -2,4 +2,4 @@
 @echo off
 cd src\
 echo the quick brown fox jumps over the lazy dog
-echo the quick brown fox jumps over the lazy dog | java -jar myApp.jar
+echo the quick brown fox jumps over the lazy dog| java -jar myApp.jar
--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,6 @@
 //Функция для сортировки
 fun sorted(list: List<String>): List<String> {
-    val ListSorted = list.sorted()
-    return ListSorted
+    return list.sorted()
 }
 
 
@@ -9,54 +8,52 @@ fun main() {
 
     val words = readLine().toString()
 //Разделение строк
-    println("-----------Первый уровень-------------------")
-    var yes = words.split(" ")
-    yes.forEach { el -> println(el) }
+    println("-----------Первый уровень----------------")
+    var listWords = words.split(" ")
+    listWords.forEach { el -> println(el) }
 
 
 //Сортировка списка слов
-    println("-----------Второй уровень---------------------")
-    yes = sorted(yes)
-    yes.forEach { el -> println(el) }
+    println("-----------Второй уровень----------------")
+    listWords = sorted(listWords)
+    listWords.forEach { el -> println(el) }
 
 //Вывод только уникальных слов
-    println("-----------Третий уровень---------------------")
+    println("-----------Третий уровень----------------")
 
     var i = 0
-    var count = yes.size - 1
+    var count = listWords.size - 1
     while (i < count) {
-        if (yes[i] != yes[i + 1]) {
-            println(yes[i])
+        if (listWords[i] != listWords[i + 1]) {
+            println(listWords[i])
 
         }
         i += 1
     }
-    println(yes.last())
+    println(listWords.last())
 
 //Подсчет количества повторений слов
-    println("----------Четвертый уровень-----------------")
+    println("----------Четвертый уровень--------------")
 
     i = 0
     var k = 0
-    val yes2 = mutableMapOf<String, Int>() // создание ассоциативного массива
+    val wordsMap = mutableMapOf<String, Int>() // создание ассоциативного массива
 
-    count = yes.size - 1
+    count = listWords.size - 1
     while (i < count) {
-        if (yes[i] != yes[i + 1]) {
-            yes2.put(yes[i], k + 1)
-
+        if (listWords[i] != listWords[i + 1]) {
+            wordsMap.put(listWords[i], k + 1)
             k = 0
         } else k += 1
-
         i += 1
     }
-    yes2.put(yes.last(), k + 1)
-    yes2.forEach { (key, value) -> println("$key $value") }
+    wordsMap.put(listWords.last(), k + 1)
+    wordsMap.forEach { (key, value) -> println("$key $value") }
 
 //сортировка слов по убыванию
     println("----------Пятый уровень----------------")
-    val result = yes2.toList().sortedByDescending { (_, value) -> value }.toMap()
-    result.forEach { key, value -> println("$key $value") }
+    val result = wordsMap.toList().sortedByDescending { (_, value) -> value }.toMap()
+    result.forEach { (key, value) -> println("$key $value") }
 
 
 }
