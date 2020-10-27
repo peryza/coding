3064046d44c82ef0b5e668f8842215efb321c67a
peryza <51073553+peryza@users.noreply.github.com>
2020-10-28 00:19:21 +0700

    #18 edit .gitignore

--- src/main.kt
+++ src/main.kt
@@ -12,34 +12,34 @@ fun work(listWords: List<String>) {
 
 /* Разделение строк - Уровень 1 */
 
-    println("-----------Первый уровень----------------")
+    println("-----------LEVEL 1----------------")
     listWords.forEach(::println)
 
 
 /* Сортировка списка слов - Уровень 2 */
 
-    println("-----------Второй уровень----------------")
+    println("-----------LEVEL 2----------------")
     val listSort = listWords.sorted()
     listSort.forEach(::println)
 
 
 /* Вывод только уникальных слов - Уровень 3 */
 
-    println("-----------Третий уровень----------------")
+    println("-----------LEVEL 3----------------")
     val listDist = listSort.distinct()
     listDist.forEach(::println)
 
 
 /* Подсчет количества повторений слов - Уровень 4 */
 
-    println("----------Четвертый уровень--------------")
+    println("-----------LEVEL 4----------------")
 
     val listWordCount = listSort.groupingBy { it }.eachCount()
     listWordCount.forEach { (word, count) -> println("$word $count") }
 
 /* сортировка слов по убыванию - Уровень 5 */
 
-    println("----------Пятый уровень----------------")
+    println("-----------LEVEL 5----------------")
 
     val listWordCountSort = listWordCount.toList().sortedWith(compareBy({ -it.second }, { it.first }))
     listWordCountSort.forEach { (word, count) -> println("$word $count") }
